d8896a73a900e0ca6872ea2a4d2471c86e8bbbd5
j-oshi <jtoshin@msn.com>
2021-08-08 10:32:21 +0100

    create first commit

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 import { 
-    stringContainOneWordOrLetter,
-    totalNumberOfDigitsInString,
-    validateOperandString,
-    findFirstOperator,
-    executeOperation,
-    standardizeVariable,
-    substituteVariableValue,
-    comparisonType,
     evaluteComparisonExpression 
 } from "./comparison-operator-string-evaluator.js";
 
-console.log(findFirstOperator('10 <= ytytfytfytg >= 13'));
-console.log(comparisonType('10 <= 20 <= 13'));
-console.log(executeOperation('10 <= 13'));
 console.log(evaluteComparisonExpression('10 <= ytytfytfytg <= 13', 13));
 console.log(evaluteComparisonExpression('10 == ytytfytf <= 700', 13));
 console.log(evaluteComparisonExpression('10 < ytytfytf <= 700', 13));
